4fc0993b5d8f7d42ff0dfbf4a1dbe77702d652bb
rambhera-source <ram.bhera@gmail.com>
2026-09-12 19:08:58 +0530

    Create user_profile.dart

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,21 @@
+import 'package:isar/isar.dart';
+
+part 'user_profile.g.dart';
+
+@collection
+class UserProfile {
+  Id id = Isar.autoIncrement;
+
+  @Index(unique: true, replace: true)
+  late String username;
+
+  late String password;
+  
+  // Business Type: 'retail', 'wholesale', ya 'manufacturing'
+  late String businessType; 
+
+  // Subscription expiry date
+  late DateTime subscriptionExpiry;
+  
+  bool isActive = true;
+}
