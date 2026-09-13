edd11731a9cdf10215160c021fd3ad98e20057cc
rambhera-source <ram.bhera@gmail.com>
2026-09-13 19:14:28 +0530

    Create order_model.dart

---
+++ lib/models/order_model.dart
@@ -0,0 +1,24 @@
+import 'isar.dart';
+
+part 'order_model.g.dart';
+
+@collection
+class OrderItemModel {
+  Id id = Isar.autoIncrement;
+  late String productName;
+  late int qty;
+  late double price;
+  bool isDelivered = false; // Agar deliver ho gaya toh true
+}
+
+@collection
+class SalesOrder {
+  Id id = Isar.autoIncrement;
+  late String orderNo;
+  late String partyName;
+  late DateTime date;
+  
+  final items = IsarLinks<OrderItemModel>();
+  
+  String status = 'Pending'; // 'Pending', 'Converted to Bill', 'Cancelled'
+}
