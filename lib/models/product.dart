531aacea492ed50c238b75dfa6e8f45f7974d3b8
rambhera-source <ram.bhera@gmail.com>
2026-09-14 10:22:52 +0530

    Create product.dart

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+import 'package:isar/isar.dart';
+
+part 'product.g.dart';
+
+@collection
+class Product {
+  Id id = Isar.autoIncrement;
+
+  @Index(unique: true, replace: true)
+  late String name;
+  double stock = 0.0;
+  double sellingPrice = 0.0;
+}
